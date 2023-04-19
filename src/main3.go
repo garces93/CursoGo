48601aac1ebd427638da6bae79f9eddcc1a723b9
Daniel <barrosgarces93@gmail.com>
2023-04-19 16:52:15 -0500

    Keywords

---
+++ src/main3.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*modulo := 5 % 2
+	switch modulo {
+	case 0:
+		fmt.Println("es par", modulo)
+	default:
+		fmt.Println("es impar", modulo)
+
+	}
+
+	// sin condicion
+
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("es mayor a 100")
+	case value < 0:
+		fmt.Println("es menor a 0")
+	default:
+		fmt.Println("no condicional")
+
+	}
+	*/
+
+	//vamos a ver los KeyWords
+
+	fmt.Println("hola")
+	fmt.Println("mundo")
+
+}
